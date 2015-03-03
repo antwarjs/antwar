73923a4b5af1b62ddae0dc0903c856bff22c746e
Juho Vepsalainen <bebraw@gmail.com>
2015-03-03 19:52:08 +0200

    Drop console.logs from entry point
    Ideally core wouldn't contain any. Better to have these outside.

--- index.js
+++ index.js
@@ -6,12 +6,10 @@ var build = require('./build.coffee');
 
 
 exports.develop = function(config) {
-	console.log('Starting dev server…');
 	build.buildDevIndex(config);
 	devServer.dev('build');
 };
 
 exports.build = function(config) {
-	console.log('Building site…');
 	build.build(config);
 };
