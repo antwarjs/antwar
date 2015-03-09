16d09f18642ec905b2a2363458289198f814604c
Juho Vepsalainen <bebraw@gmail.com>
2015-03-09 15:15:44 +0200

    Flatten promise structures
    I had extra nesting there that could be dropped.

--- config/build.js
+++ config/build.js
@@ -7,64 +7,60 @@ var getCommon = require('./common');
 
 
 module.exports = function(config) {
-  return new Promise(function(resolve, reject) {
-    var cwd = process.cwd();
+  var cwd = process.cwd();
 
-    getCommon(config).then(function(common) {
-      resolve({
-        name: 'server',
-        target: 'node',
-        context: path.join(__dirname, '..', './'),
-        entry: {
-          bundlePage: './dev/page.js',
-          // XXX: inject via rss via a plugin
-          //bundleStaticRss: './dev/staticRss.js',
-          bundleStaticPage: './dev/staticPage.js',
-          paths: './dev/exportPaths.js',
-        },
-        output: {
-          path: path.join(cwd, './.antwar/build'),
-          filename: '[name].js',
-          publicPath: path.join(cwd, './.antwar/build'),
-          libraryTarget: 'commonjs2',
-        },
-        plugins: [
-          new ExtractTextPlugin('main.css', {
-            allChunks: true,
-          })
+  return getCommon(config).then(function(common) {
+    return {
+      name: 'server',
+      target: 'node',
+      context: path.join(__dirname, '..', './'),
+      entry: {
+        bundlePage: './dev/page.js',
+        // XXX: inject via rss via a plugin
+        //bundleStaticRss: './dev/staticRss.js',
+        bundleStaticPage: './dev/staticPage.js',
+        paths: './dev/exportPaths.js',
+      },
+      output: {
+        path: path.join(cwd, './.antwar/build'),
+        filename: '[name].js',
+        publicPath: path.join(cwd, './.antwar/build'),
+        libraryTarget: 'commonjs2',
+      },
+      plugins: [
+        new ExtractTextPlugin('main.css', {
+          allChunks: true,
+        })
+      ],
+      resolve: common.resolve,
+      resolveLoader: common.resolveLoader,
+      module: {
+        loaders: [
+          {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(
+              'style-loader',
+              'css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
+          },
+          {
+            test: /\.jsx?$/,
+            loader: 'jsx-loader?harmony',
+          },
+          {
+            test: /\.coffee$/,
+            loader: 'coffee-loader',
+          },
+          {
+            test: /\.html$/,
+            loader: 'raw',
+          },
+          {
+            test: /\.md$/,
+            loader: 'json!yaml-frontmatter-loader',
+          }
         ],
-        resolve: common.resolve,
-        resolveLoader: common.resolveLoader,
-        module: {
-          loaders: [
-            {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract(
-                'style-loader',
-                'css-loader!autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}!sass-loader'),
-            },
-            {
-              test: /\.jsx?$/,
-              loader: 'jsx-loader?harmony',
-            },
-            {
-              test: /\.coffee$/,
-              loader: 'coffee-loader',
-            },
-            {
-              test: /\.html$/,
-              loader: 'raw',
-            },
-            {
-              test: /\.md$/,
-              loader: 'json!yaml-frontmatter-loader',
-            }
-          ],
-          jshint: common.jshint,
-        }
-      });
-    }).catch(function(err) {
-      reject(err);
-    });
+        jshint: common.jshint,
+      }
+    };
   });
 };
--- config/dev.js
+++ config/dev.js
@@ -6,82 +6,77 @@ var getCommon = require('./common');
 
 module.exports = function(config) {
   var ReactHotLoaderMatches = /View.coffee|Pages\//;
+  var cwd = process.cwd();
 
-  return new Promise(function(resolve, reject) {
-    var cwd = process.cwd();
-
-    getCommon(config).then(function(common) {
-      resolve({
-        cache: true,
-        node: {
-          __filename: true
-        },
-        output: {
-          path: path.join(cwd, './.antwar/build/'),
-          publicPath: '/',
-          filename: '[name]-bundle.js',
-          chunkFilename: '[chunkhash].js',
-        },
-        plugins: [],
-        module: {
-          loaders: [
-            {
-              test: /\.woff$/,
-              loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff',
-            },
-            {
-              test: /\.ttf$|\.eot$/,
-              loader: 'file-loader?prefix=font/',
-            },
-            {
-              test: /\.coffee$/,
-              exclude: ReactHotLoaderMatches,
-              loader: 'react-hot!jshint-loader!coffee-loader',
-            },
-            {
-              test: /\.json$/,
-              loader: 'json-loader',
-            },
-            {
-              test: /\.svg$/,
-              loader: 'raw-loader',
-            },
-            {
-              test: /\.jsx?$/,
-              loader: 'jsx-loader?harmony',
-            },
-            {
-              test: /\.css$/,
-              loaders: [
-                'style-loader',
-                'css-loader',
-              ],
-            },
-            {
-              test: /\.scss$/,
-              loaders: [
-                'style-loader',
-                'css-loader',
-                'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
-                'sass-loader',
-              ],
-            },
-            {
-              test: ReactHotLoaderMatches,
-              loader: 'react-hot!coffee-loader',
-            },
-            {
-              test: /\.md$/,
-              loader: 'json!yaml-frontmatter-loader',
-            }
-          ],
-        },
-        resolve: common.resolve,
-        resolveLoader: common.resolveLoader,
-        jshint: common.jshint,
-      });
-    }).catch(function(err) {
-      reject(err);
-    });
+  return getCommon(config).then(function(common) {
+    return {
+      cache: true,
+      node: {
+        __filename: true
+      },
+      output: {
+        path: path.join(cwd, './.antwar/build/'),
+        publicPath: '/',
+        filename: '[name]-bundle.js',
+        chunkFilename: '[chunkhash].js',
+      },
+      plugins: [],
+      module: {
+        loaders: [
+          {
+            test: /\.woff$/,
+            loader: 'url-loader?prefix=font/&limit=5000&mimetype=application/font-woff',
+          },
+          {
+            test: /\.ttf$|\.eot$/,
+            loader: 'file-loader?prefix=font/',
+          },
+          {
+            test: /\.coffee$/,
+            exclude: ReactHotLoaderMatches,
+            loader: 'react-hot!jshint-loader!coffee-loader',
+          },
+          {
+            test: /\.json$/,
+            loader: 'json-loader',
+          },
+          {
+            test: /\.svg$/,
+            loader: 'raw-loader',
+          },
+          {
+            test: /\.jsx?$/,
+            loader: 'jsx-loader?harmony',
+          },
+          {
+            test: /\.css$/,
+            loaders: [
+              'style-loader',
+              'css-loader',
+            ],
+          },
+          {
+            test: /\.scss$/,
+            loaders: [
+              'style-loader',
+              'css-loader',
+              'autoprefixer-loader?{browsers:["last 2 version", "ie 10", "Android 4"]}',
+              'sass-loader',
+            ],
+          },
+          {
+            test: ReactHotLoaderMatches,
+            loader: 'react-hot!coffee-loader',
+          },
+          {
+            test: /\.md$/,
+            loader: 'json!yaml-frontmatter-loader',
+          }
+        ],
+      },
+      resolve: common.resolve,
+      resolveLoader: common.resolveLoader,
+      jshint: common.jshint,
+    };
   });
 };
