b1ac2975de5ae6dd2e28bd85e99a7fa428a4c859
Juho Vepsalainen <bebraw@gmail.com>
2015-03-09 16:09:16 +0200

    Push build core to a module of its own

--- build/index.js
+++ build/index.js
@@ -1,56 +1,5 @@
 'use strict';
-var _path = require('path');
 
-var webpack = require('webpack');
-
-var webpackConfig = require('../config/build');
-var write = require('./write');
-
-
-module.exports = function(config) {
-  process.env.NODE_ENV = 'production';
-
-  return new Promise(function(resolve, reject) {
-    var site = config.site;
-
-    if(!site) {
-      return reject(new Error('Missing site configuration'));
-    }
-
-    var output = site.output;
-
-    if(!output) {
-      return reject(new Error('Missing output directory'));
-    }
-
-    webpackConfig(config).then(function(c) {
-      webpack(c, function(err) {
-        if(err) {
-          return reject(err);
-        }
-
-        var cwd = process.cwd();
-        var params = {
-          cwd: cwd,
-          renderPage: require(_path.join(cwd, './.antwar/build/bundleStaticPage.js')),
-          allPaths: require(_path.join(cwd, './.antwar/build/paths.js'))(),
-          output: _path.join(cwd, output),
-        };
-        write.assets(params);
-        write.pages(params);
-        write.index(params);
-        write.posts(params);
-
-        // TODO: this should be pushed to a plugin
-        //var rss = require(_path.join(cwd, './.antwar/build/bundleStaticRss.js'));
-        //fs.writeFileSync(_path.join(process.cwd(), output, 'atom.xml'), rss(renderPage));
-
-        resolve();
-      });
-    }).catch(function(err) {
-      reject(err);
-    });
-  });
-};
+module.exports = require('./build');
 module.exports.devIndex = require('./dev_index');
 module.exports.devServer = require('./dev_server');
