92ae21e2a9b9531a232e1a1cdf51ee4d5bcba460
Andreas Eldh <andreas.eldh@gmail.com>
2015-03-13 17:45:54 +0100

    Posts hooks for plugins.
    Still not tested with require(), but works for pure obj.

--- postHooks.js
+++ postHooks.js
@@ -19,16 +19,13 @@ var getFunctions = function (hookName) {
   if (themeFunctions[hookName]) {
     functions.push(themeFunctions[hookName]);
   }
-
-  // if (config.plugins) {
-  //   _.each(config.plugins, function (pluginName) {
-  //     var plugin = require(pluginName);
-  //     if(plugin && plugin.functions && plugin.functions[hookName]) {
-  //       functions.push(plugin.functions[hookName]);
-  //     }
-  //   });
-  // }
-
+  if (config.site.plugins) {
+    _.each(config.site.plugins, function (plugin) {
+      if(plugin.functions && plugin.functions[hookName]) {
+        functions.push(plugin.functions[hookName]);
+      }
+    });
+  }
   if (config.site.functions && config.site.functions[hookName]) {
     functions.push(config.site.functions[hookName]);
   }
