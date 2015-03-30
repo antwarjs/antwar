aa97a6be040c14af0e2f9a35bc9e15e32b288557
Juho Vepsalainen <bebraw@gmail.com>
2015-03-30 11:51:08 +0300

    Set up advanced test script for travis
    Not sure if this works yet.

---
+++ tests/travis.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e # exit with nonzero exit code if anything fails
+
+# link the library so cli can find it
+npm link
+
+# clear and re-create the test directory
+rm -rf test || exit 0
+mkdir test
+
+cd test
+
+# clone cli and set it up
+git clone git@github.com:antwarjs/cli.git .
+npm link
+
+# remove installed version given we want to rely on the linked one
+rm -rf node_modules/antwar
+
+# initialize project and build it
+cd ..
+antwar -i demo
+cd demo
+antwar -b
