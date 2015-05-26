a5861a0550bf04ad0df8477bcc189ac3eed9fd02
Juho Vepsalainen <bebraw@gmail.com>
2015-05-26 20:59:53 +0300

    Support copying individual files for asset conf
    Ie. just have
    
    ```javascript
    {
      from: './CNAME',
      to: './',
    }
    ```
    
    and it will copy `from` file into `to` directory within output.

--- build/utils.js
+++ build/utils.js
@@ -3,12 +3,20 @@ var path = require('path');
 
 var async = require('async');
 var cpr = require('cpr');
+var cp = require('cp');
 
 
 exports.copyExtraAssets = function(buildDir, assets, cb) {
   assets = assets || [];
 
   async.forEach(assets, function(asset, cb) {
-    cpr(asset.from, path.join(buildDir, asset.to), cb);
+    var from = asset.from;
+
+    if(from.indexOf('./') === 0) {
+        cp(from, path.join(buildDir, asset.to, from), cb);
+    }
+    else {
+        cpr(from, path.join(buildDir, asset.to), cb);
+    }
   }, cb);
 };
