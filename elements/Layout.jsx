4f83ea18508da3ee27c62da9f6e54596c37dfaac
Juho Vepsalainen <bebraw@gmail.com>
2015-05-26 20:19:39 +0300

    Improve formatting

--- elements/Layout.jsx
+++ elements/Layout.jsx
@@ -30,25 +30,27 @@ module.exports = React.createClass({
     pageTitle = pageTitle && pageTitle + ' / ';
     pageTitle += siteName;
 
-    return <html>
-      <head>
-        <title>{pageTitle}</title>
-        <meta
-          name='viewport'
-          content='width=device-width, initial-scale=1, maximum-scale=1, minimal-ui'>
-        </meta>
-        <link rel="icon" type="image/png" href="assets/img/favicon.png"></link>
-        {_.map(this.getExternalHeadContent(this.getAllItems()), function (Component, i) {
-          return <Component key={'component-' + i} />;
-        })}
-        {!__DEV__?
-          <link rel='stylesheet' href='assets/main.css'></link>:
-          null}
-      </head>
-      <body>
-        <RouteHandler></RouteHandler>
-        {__DEV__ ? <script src='/main-bundle.js'></script> : null}
-      </body>
-    </html>;
+    return (
+      <html>
+        <head>
+          <title>{pageTitle}</title>
+          <meta
+            name='viewport'
+            content='width=device-width, initial-scale=1, maximum-scale=1, minimal-ui'>
+          </meta>
+          <link rel="icon" type="image/png" href="assets/img/favicon.png"></link>
+          {_.map(this.getExternalHeadContent(this.getAllItems()), function (Component, i) {
+            return <Component key={'component-' + i} />;
+          })}
+          {!__DEV__?
+            <link rel='stylesheet' href='assets/main.css'></link>:
+            null}
+        </head>
+        <body>
+          <RouteHandler></RouteHandler>
+          {__DEV__ ? <script src='/main-bundle.js'></script> : null}
+        </body>
+      </html>
+    );
   }
 });
