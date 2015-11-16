db76dd904bb22f9789f2cd4c7b8b5f6b0d29433e
Juho Vepsalainen <bebraw@gmail.com>
2015-11-16 13:06:29 +0200

    Tidy up

--- elements/Routes.jsx
+++ elements/Routes.jsx
@@ -1,20 +1,16 @@
 'use strict';
-var _ = require('lodash');
 var React = require('react');
 var Router = require('react-router');
 var Route = Router.Route;
 
 var Layout = require('./Layout.jsx');
-
 var DevIndex = require('./DevIndex.jsx');
 var BodyRoutes = require('./BodyRoutes.jsx');
 
-var Routes = (
+// XXXXX: allow custom root layout to be passed
+export default (
   <Route name='home' title='Home' handler={Layout}>
     <Route name='/antwar_devindex' handler={DevIndex}></Route>
     {BodyRoutes}
   </Route>
 );
-
-
-module.exports = Routes
