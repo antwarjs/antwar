d0759aee8e740253950f24e315a1b381fe50505e
Juho Vepsalainen <bebraw@gmail.com>
2015-11-16 14:48:36 +0200

    Drop XXXXX
    This has been solved through configuration.

--- elements/Routes.jsx
+++ elements/Routes.jsx
@@ -7,7 +7,6 @@ var Layout = require('./Layout.jsx');
 var DevIndex = require('./DevIndex.jsx');
 var BodyRoutes = require('./BodyRoutes.jsx');
 
-// XXXXX: allow custom root layout to be passed
 export default (
   <Route name='home' title='Home' handler={Layout}>
     <Route name='/antwar_devindex' handler={DevIndex}></Route>
