3b1dde6c75162ab6668a0bee831a10e67fa15a1c
Juho Vepsalainen <bebraw@gmail.com>
2015-11-19 11:50:51 +0200

    Stringify

--- config/common.js
+++ config/common.js
@@ -46,7 +46,7 @@ module.exports = function(config) {
         new webpack.DefinePlugin({
           __DEV__: JSON.stringify(JSON.parse(config.buildDev)),
           'process.env': {
-            'NODE_ENV': 'dev'
+            'NODE_ENV': JSON.stringify('dev')
           }
         }),
         new webpack.optimize.OccurenceOrderPlugin(),
