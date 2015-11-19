a813ba2b2295e1c6bc21ad2bec881b31c0f09181
Juho Vepsalainen <bebraw@gmail.com>
2015-11-19 10:16:12 +0200

    Merge BodyRoutes with Routes
    Far simpler this way.

--- components/Routes.jsx
+++ components/Routes.jsx
@@ -1,14 +1,28 @@
-'use strict';
-var React = require('react');
-var Router = require('react-router');
-var Route = Router.Route;
-var DevIndex = require('./DevIndex.jsx');
-var BodyContent = require('./BodyContent.jsx');
-var BodyRoutes = require('./BodyRoutes.jsx');
+import _ from 'lodash';
+import React from 'react';
+import {Route} from 'react-router';
+import DevIndex from './DevIndex.jsx';
+import BodyContent from './BodyContent.jsx';
+import paths from '../libs/paths';
+import config from 'config';
 
 export default (
   <Route component={BodyContent}>
-    <Route path="antwar_devindex" component={DevIndex} />
-    {BodyRoutes}
+    {__DEV__ && <Route path="antwar_devindex" component={DevIndex} />}
+    {_.map(config.paths, function(v, k) {
+      const pathRoutes = paths.getSectionPages(k).map(_.property('url')).map((url) =>
+        <Route component={BodyContent} path={url} />
+      );
+
+      // possible root path needs to be after other paths. else it can match too early
+      if(k === '/') {
+        return pathRoutes;
+      }
+
+      return pathRoutes.concat([
+        <Route component={BodyContent} path={k} />
+      ]);
+    })}
+    {config.paths['/'] && <Route component={BodyContent} path="/" />}
   </Route>
 );
