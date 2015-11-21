9ebaf78c21b98fb221f99091794b6f52e6a46e3f
Juho Vepsalainen <bebraw@gmail.com>
2015-11-21 11:03:38 +0200

    DevIndex - Simplify implementation
    I changed this to use React's function based component definition.

--- components/DevIndex.jsx
+++ components/DevIndex.jsx
@@ -1,24 +1,20 @@
-'use strict';
-var React = require('react');
+import React from 'react';
 import config from 'config';
 
-module.exports = React.createClass({
-  displayName: 'DevIndex',
-  render() {
-    const props = {
-      config,
-      section: {
-        pages: () => {return [];}
-      },
-      page: {},
-      location: this.props.location
-    };
-    const Body = config.layout();
+export default ({location}) => {
+  const props = {
+    config,
+    section: {
+      pages: () => []
+    },
+    page: {},
+    location
+  };
+  const Body = config.layout();
 
-    return (
-      <Body {...props}>
-        <div id="dev-container" />
-      </Body>
-    );
-  }
-});
+  return (
+    <Body {...props}>
+      <div id="dev-container" />
+    </Body>
+  );
+};
