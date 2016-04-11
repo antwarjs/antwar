af3e90a813279c8bb88e08cb9b3f47cb2e70cf11
Juho Vepsalainen <bebraw@gmail.com>
2016-04-11 09:35:17 +0300

    Port DevIndex to class style
    Perhaps more stable this way.

--- components/DevIndex.jsx
+++ components/DevIndex.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import config from 'config';
 
-export default ({location}) => {
-  const props = {
-    config,
-    section: {
-      pages: () => []
-    },
-    page: {},
-    location
-  };
-  const Body = config.layout();
+export default class DevIndex extends React.Component {
+  render() {
+    const props = {
+      config,
+      section: {
+        pages: () => []
+      },
+      page: {},
+      location: this.props.location
+    };
+    const Body = config.layout();
 
-  return (
-    <Body {...props}>
-      <div id="dev-container" />
-    </Body>
-  );
+    return (
+      <Body {...props}>
+        <div id="dev-container" />
+      </Body>
+    );
+  }
 };
