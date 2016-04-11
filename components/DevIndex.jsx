e3f535f4f03d82a52ea29d4c13cc3465801fd1c6
Juho Vepsalainen <bebraw@gmail.com>
2016-04-11 11:15:12 +0300

    Revert back to an earlier version of `DevIndex`
    Still errors to resolve.

--- components/DevIndex.jsx
+++ components/DevIndex.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
 import config from 'config';
 
-export default class DevIndex extends React.Component {
-  render() {
-    const props = {
-      config,
-      section: {
-        pages: () => []
-      },
-      page: {},
-      location: this.props.location
-    };
-    const Body = config.layout();
+export default ({location}) => {
+  const props = {
+    config,
+    section: {
+      pages: () => []
+    },
+    page: {},
+    location
+  };
+  const Body = config.layout();
 
-    return (
-      <Body {...props}>
-        <div id="dev-container" />
-      </Body>
-    );
-  }
+  return (
+    <Body {...props}>
+      <div id="dev-container" />
+    </Body>
+  );
 };
