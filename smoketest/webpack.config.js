9629f6443a1a39c20b69cbb78d1bc6555cfa6f19
Juho Vepsalainen <bebraw@gmail.com>
2016-09-13 09:42:00 +0300

    smoketest - Add initial styling setup

--- smoketest/webpack.config.js
+++ smoketest/webpack.config.js
@@ -1,18 +1,74 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const merge = require('webpack-merge');
 
-module.exports = function () {
+const PATHS = {
+  style: [
+    path.join(__dirname, 'style', 'main.css')
+  ]
+};
+
+const commonConfig = {
+  entry: {
+    style: PATHS.style
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        include: [
+          path.join(__dirname, 'layouts'),
+          path.join(__dirname, 'pages')
+        ]
+      }
+    ]
+  }
+};
+
+module.exports = function (env) {
+  switch (env) {
+    case 'build':
+      return merge(
+        commonConfig,
+        buildConfig(PATHS.style)
+      );
+    case 'start':
+    default:
+      return merge(
+        commonConfig,
+        developmentConfig(PATHS.style)
+      );
+  }
+};
+
+function developmentConfig(stylePaths) {
   return {
     module: {
       loaders: [
         {
-          test: /\.jsx?$/,
-          loader: 'babel-loader',
-          include: [
-            path.join(__dirname, 'layouts'),
-            path.join(__dirname, 'pages')
-          ]
+          test: /\.css$/,
+          loaders: ['style-loader', 'css-loader'],
+          include: stylePaths
         }
       ]
     }
   };
-};
+}
+
+function buildConfig(stylePaths) {
+  return {
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract(
+            'style-loader',
+            'css-loader'
+          ),
+          include: stylePaths
+        }
+      ]
+    }
+  };
+}
