8ac8606bbd64088f39b7b1cab25972ac125bc3bd
Juho Vepsalainen <bebraw@gmail.com>
2016-09-13 16:13:30 +0300

    smoketest - Configure extract-text-plugin correctly
    Missing plugin part.

--- smoketest/webpack.config.js
+++ smoketest/webpack.config.js
@@ -69,6 +69,9 @@ function buildConfig(stylePaths) {
           include: stylePaths
         }
       ]
-    }
+    },
+    plugins: [
+      new ExtractTextPlugin('[name].[chunkhash].css')
+    ]
   };
 }
