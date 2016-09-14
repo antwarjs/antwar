30511046183bc9117482198e68a2b45c7c4e106c
Juho Vepsalainen <bebraw@gmail.com>
2016-09-14 08:39:21 +0300

    antwar - Promisify build logic

--- packages/antwar/src/build/build.js
+++ packages/antwar/src/build/build.js
@@ -23,61 +23,25 @@ module.exports = function (config) {
       return reject(new Error('Missing output directory'));
     }
 
-    return webpackConfig(config).then(function (c) {
-      webpack(c, function (err, stats) {
-        if (err) {
-          return reject(err);
-        }
-
-        if (stats.hasErrors()) {
-          return reject(stats.toString('errors-only'));
-        }
-
-        // XXXXX: Capture files ending with .css from stats.compilation.assets
-        // here and inject them to template context. In addition the CSS files
-        // need to be copied to project root based on existsAt path.
-
-        const cwd = process.cwd();
-        const params = {
-          cwd,
-          renderPage: require(
-            _path.join(cwd, './.antwar/build/bundleStaticPage.js')
-          ),
-          allPaths: require(
-            _path.join(cwd, './.antwar/build/paths.js')
-          )(),
-          output: _path.join(cwd, output),
-          config: config.antwar,
-          template: {
-            ...config.antwar.template,
-            // XXX: sync operation
-            file: _fs.readFileSync(
-              (config.antwar.template && config.antwar.template.file) ||
-              _path.join(__dirname, '../../template.ejs'),
-              {
-                encoding: 'utf8'
-              }
-            )
-          }
-        };
-
-        log('Removing old output directory');
-
-        return removeDirectory(params.output)
-          .then(writeExtras.bind(null, config, params))
-          .then(executeTasks.bind(null, log));
-      });
-    }).catch(function (err) {
-      reject(err);
-    });
+    return webpackConfig(config)
+      .then(runWebpack)
+      .then(generateParameters.bind(null, config))
+      .then(removeDirectory.bind(null, log))
+      .then(writeExtras.bind(null, config))
+      .then(executeTasks.bind(null, log))
+      .catch(reject);
   });
 };
 
-function removeDirectory(directory) {
+function runWebpack(config) {
   return new Promise(function (resolve, reject) {
-    rimraf(directory, function (err) {
+    webpack(config, function (err, stats) {
       if (err) {
         return reject(err);
+      }
+
+      if (stats.hasErrors()) {
+        return reject(stats.toString('errors-only'));
       }
 
       return resolve();
@@ -85,21 +49,70 @@ function removeDirectory(directory) {
   });
 }
 
-function writeExtras(config, params) {
+function generateParameters(config) {
+  return new Promise(function (resolve) {
+    // XXXXX: Capture files ending with .css from stats.compilation.assets
+    // here and inject them to template context. In addition the CSS files
+    // need to be copied to project root based on existsAt path.
+
+    const output = config.antwar.output;
+    const cwd = process.cwd();
+    const parameters = {
+      cwd,
+      renderPage: require(
+        _path.join(cwd, './.antwar/build/bundleStaticPage.js')
+      ),
+      allPaths: require(
+        _path.join(cwd, './.antwar/build/paths.js')
+      )(),
+      output: _path.join(cwd, output),
+      config: config.antwar,
+      template: {
+        ...config.antwar.template,
+        // XXX: sync operation
+        file: _fs.readFileSync(
+          (config.antwar.template && config.antwar.template.file) ||
+          _path.join(__dirname, '../../template.ejs'),
+          {
+            encoding: 'utf8'
+          }
+        )
+      }
+    };
+
+    resolve(parameters);
+  });
+}
+
+function removeDirectory(log, parameters) {
+  return new Promise(function (resolve, reject) {
+    log('Removing old output directory');
+
+    rimraf(parameters.output, function (err) {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(parameters);
+    });
+  });
+}
+
+function writeExtras(config, parameters) {
   return new Promise(function (resolve, reject) {
     // Extras
     const pluginExtras = _.map(config.antwar.plugins, 'extra').filter(_.identity);
     const extraFiles = _.map(pluginExtras, function (plugin) {
-      return plugin(params.allPaths, config.antwar);
+      return plugin(parameters.allPaths, config.antwar);
     });
 
     // get functions to execute
     return async.parallel([
-      write.assets.bind(null, params),
-      write.extraAssets.bind(null, params),
-      write.extras.bind(null, params, extraFiles),
-      write.pages.bind(null, params),
-      write.redirects.bind(null, params)
+      write.assets.bind(null, parameters),
+      write.extraAssets.bind(null, parameters),
+      write.extras.bind(null, parameters, extraFiles),
+      write.pages.bind(null, parameters),
+      write.redirects.bind(null, parameters)
     ], function (err, tasks) {
       if (err) {
         return reject(err);
