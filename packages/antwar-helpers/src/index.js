4f6a3ea48eacd5562452bcd012119940e556df3c
Juho Vepsalainen <bebraw@gmail.com>
2016-09-15 14:21:31 +0300

    helpers - Rewrite root export in ES6 style

--- packages/antwar-helpers/src/index.js
+++ packages/antwar-helpers/src/index.js
@@ -1,12 +1,10 @@
-module.exports = {
-  Disqus: require('./Disqus'),
-  Gitter: require('./Gitter'),
-  GoogleAnalytics: require('./GoogleAnalytics'),
-  Navigation: require('./Navigation'),
-  Markdown: require('./Markdown'),
-  Moment: require('./Moment'),
-  PrevNext: require('./PrevNext'),
-  PrevNextMini: require('./PrevNextMini'),
-  Teaser: require('./Teaser'),
-  Toc: require('./Toc')
-};
+export { default as Disqus } from './Disqus';
+export { default as Gitter } from './Gitter';
+export { default as GoogleAnalytics } from './GoogleAnalytics';
+export { default as Navigation } from './Navigation';
+export { default as Markdown } from './Markdown';
+export { default as Moment } from './Moment';
+export { default as PrevNext } from './PrevNext';
+export { default as PrevNextMini } from './PrevNextMini';
+export { default as Teaser } from './Teaser';
+export { default as Toc } from './Toc';
