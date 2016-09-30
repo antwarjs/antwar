891a70723203143d9d73b1424f84866d2f899b78
Juho Vepsalainen <bebraw@gmail.com>
2016-09-30 14:59:25 +0300

    interactive - Add stub

---
+++ packages/antwar-interactive/src/Interactive.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Interactive = () => (
+  <div>Interactive</div>
+);
+
+export default Interactive;
--- smoketest/layouts/Index.jsx
+++ smoketest/layouts/Index.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { Markdown } from 'antwar-helpers';
+import Interactive from 'antwar-interactive';
 
 const Index = ({ page }) => (
   <div>
     <h1>{page.title}</h1>
+
+    <Interactive />
 
     <Markdown page={page} />
   </div>
