89913e0a3a1c6aacb4aa9e6639b60740f05f5666
Juho Vepsalainen <bebraw@gmail.com>
2016-10-02 18:58:48 +0300

    interactive - Make `Interactive` wrapper work during dev mode
    Still need to add build bits (i.e., bundle and link).

--- packages/antwar-interactive/src/Interactive.jsx
+++ packages/antwar-interactive/src/Interactive.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const Interactive = () => (
-  <div>Interactive</div>
-);
+const Interactive = ({ id, component }) => {
+  if (__DEV__) {
+    return <div>{React.createElement(component)}</div>;
+  }
+
+  return <div id={id} />;
+};
+Interactive.propTypes = {
+  id: React.PropTypes.string.isRequired,
+  component: React.PropTypes.any.isRequired
+};
 
 export default Interactive;
---
+++ smoketest/layouts/ClickMe.jsx
@@ -0,0 +1,6 @@
+/* eslint-ignore no-alert */
+import React from 'react';
+
+const ClickMe = () => <div onClick={() => alert('clicked')}>Click me</div>;
+
+export default ClickMe;
