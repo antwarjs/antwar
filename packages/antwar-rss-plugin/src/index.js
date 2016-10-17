50699ca39df6b0fec1d87cdb42d5f395bf62485d
Juho Vepsalainen <bebraw@gmail.com>
2016-10-17 12:29:46 +0300

    rss - Fix interface to work with the current version

--- packages/antwar-rss-plugin/src/index.js
+++ packages/antwar-rss-plugin/src/index.js
@@ -4,13 +4,13 @@ const moment = require('moment');
 // Antwar wrapper
 module.exports = function (o) {
   return {
-    extra(paths, config) {
+    extra: function (pages, config) { // eslint-disable-line object-shorthand
       return {
         'atom.xml': generate(
           o.baseUrl,
           o.sections,
           moment().format(),
-          paths.pages,
+          pages,
           config
         )
       };
