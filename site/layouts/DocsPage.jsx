e7f061b28fae09bf9c5d9aa0d84d8e600c2c485c
Michael Mullins <michael@webdesserts.com>
2016-11-20 20:55:02 -0600

    get documentation pages readable

--- site/layouts/DocsPage.jsx
+++ site/layouts/DocsPage.jsx
@@ -2,47 +2,38 @@ import React from 'react';
 import { Link } from 'react-router';
 import _ from 'lodash';
 
+import classes from './DocsPage.scss';
+
 const DocsPage = ({ section, page }) => (
-  <div className="post">
-    <div
-      className={
-        'docs-nav__wrapper' + (page.headerImage ? ' docs-nav__wrapper--push-down' : '')
-      }
-    >
-      <h4 className="docs-nav--header">{page.title || 'Documentation'}</h4>
-      <div className="docs-nav">{_.map(section.pages(), (navPage, i) => (
+  <div className={classes.post}>
+    <div className={classes.navWrapper} >
+      <div className={classes.nav}>{_.map(section.pages(), (navPage, i) => (
         navPage.title === page.title ?
-          <span key={`navPage-${i}`} className="docs-nav__link docs-nav__link--current">
+          <span key={`navPage-${i}`} className={classes.navLink_active}>
             {navPage.title}
           </span> :
-          <Link key={`navPage-${i}`} className="docs-nav__link" to={'/' + navPage.url}>
-            {navPage.title}
-          </Link>
+            <Link key={`navPage-${i}`} className={classes.navLink} to={'/' + navPage.url}>
+              {navPage.title}
+            </Link>
       ))}</div>
     </div>
 
     {page.headerImage ?
-      <div
-        className="header-image"
-        style={{
-          backgroundImage: `url(${page.headerImage})`
-        }}
-      /> :
+      <div className={classes.headerImage} style={{ backgroundImage: `url(${page.headerImage})` }} /> :
       null
     }
 
-    <h1 className="post__heading">{page.title}</h1>
-
-    <div className="post__content">
+    <div className={classes.postContent}>
+      <h1 className={classes.postHeading}>{page.title}</h1>
       {page.isDraft ?
-        <span className="draft-text">Draft</span> :
+        <span className={classes.draftText}>Draft</span> :
         null
       }
       <div dangerouslySetInnerHTML={{ __html: page.content }} />
     </div>
 
     {page.author ?
-      <div className="post__author">{`Authored by ${page.author}`}</div> :
+      <div className={classes.postAuthor}>{`Authored by ${page.author}`}</div> :
       null
     }
   </div>
