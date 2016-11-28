62552b2d59c1aecde1d970c437380fea6612e20e
Juho Vepsalainen <bebraw@gmail.com>
2016-11-28 17:40:33 +0200

    fix(site) - Set up `CopyWebpackPlugin` instead of assets
    Now this works as expected.

--- site/webpack.config.js
+++ site/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const merge = require('webpack-merge');
 const autoprefixer = require('autoprefixer');
 
@@ -59,6 +60,14 @@ const commonConfig = {
   },
   postcss: [
     autoprefixer({ browsers: ['last 2 versions'] })
+  ],
+  plugins: [
+    new CopyWebpackPlugin([
+      {
+        from: './CNAME',
+        to: './'
+      }
+    ])
   ]
 };
 
