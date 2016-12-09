78cdd177b6eb807e9df858dc5c679298675f9e5d
Juho Vepsalainen <bebraw@gmail.com>
2016-12-09 15:44:22 +0200

    fix(antwar) - Set up HardSourceWebpackPlugin separately for prod/dev
    Otherwise they might try to share the same cache and that won't work.

--- packages/antwar/src/config/build.js
+++ packages/antwar/src/config/build.js
@@ -1,10 +1,13 @@
-const path = require('path');
-const merge = require('webpack-merge');
+import * as path from 'path';
+import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
+import merge from 'webpack-merge';
 
 module.exports = function (config) {
   const getCommon = require('./common');
 
-  const buildPath = path.join(process.cwd(), config.antwar.output);
+  const cwd = process.cwd();
+  const buildPath = path.join(cwd, config.antwar.output);
+  const antwarPath = path.join(cwd, './.antwar');
 
   config.buildDev = config.buildDev || 0; // eslint-disable-line no-param-reassign
 
@@ -23,7 +26,18 @@ module.exports = function (config) {
         filename: '[name].js',
         publicPath: '/',
         libraryTarget: 'commonjs2'
-      }
+      },
+      plugins: [
+        new HardSourceWebpackPlugin({
+          cacheDirectory: path.join(antwarPath, 'build-cache'),
+          recordsPath: path.join(antwarPath, 'build-cache', 'records.json'),
+          environmentHash: {
+            root: cwd,
+            directories: ['node_modules'],
+            files: ['package.json']
+          }
+        })
+      ]
     };
 
     return merge(commonConfig, buildConfig, config.webpack);
--- packages/antwar/src/config/dev.js
+++ packages/antwar/src/config/dev.js
@@ -1,10 +1,12 @@
-const path = require('path');
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const getCommon = require('./common');
+import * as path from 'path';
+import merge from 'webpack-merge';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
+import getCommon from './common';
 
 module.exports = function (config) {
   const cwd = process.cwd();
+  const antwarPath = path.join(cwd, './.antwar');
 
   return getCommon(config).then(function (commonConfig) {
     const devConfig = {
@@ -24,6 +26,15 @@ module.exports = function (config) {
         new HtmlWebpackPlugin({
           template: (config.antwar.template && config.antwar.template.file) ||
             path.join(__dirname, '../../templates/page.ejs')
+        }),
+        new HardSourceWebpackPlugin({
+          cacheDirectory: path.join(antwarPath, 'dev-cache'),
+          recordsPath: path.join(antwarPath, 'dev-cache', 'records.json'),
+          environmentHash: {
+            root: cwd,
+            directories: ['node_modules'],
+            files: ['package.json']
+          }
         })
       ],
       // Copy template configuration to webpack side so HtmlWebpackPlugin picks it up
