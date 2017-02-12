9a741d4fffdf9bb89a6cda3973451cb2f97c2099
Juho Vepsalainen <bebraw@gmail.com>
2017-02-12 19:27:19 +0200

    chore - Drop HardSourcePlugin
    A little simpler this way.

--- packages/antwar/src/config/build.js
+++ packages/antwar/src/config/build.js
@@ -1,17 +1,10 @@
 import * as path from 'path';
-import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
 import merge from 'webpack-merge';
 
 module.exports = function (config) {
-  const getCommon = require('./common');
-
-  const cwd = process.cwd();
-  const buildPath = path.join(cwd, config.antwar.output);
-  const antwarPath = path.join(cwd, '.antwar');
-
   config.buildDev = config.buildDev || 0; // eslint-disable-line no-param-reassign
 
-  return getCommon(config).then(function (commonConfig) {
+  return require('./common')(config).then(function (commonConfig) {
     const buildConfig = {
       node: {
         fs: 'empty'
@@ -22,22 +15,11 @@ module.exports = function (config) {
         site: path.join(__dirname, '../build/site.js')
       },
       output: {
-        path: buildPath,
+        path: path.join(process.cwd(), config.antwar.output),
         filename: '[name].js',
         publicPath: '/',
         libraryTarget: 'commonjs2'
-      },
-      plugins: [
-        new HardSourceWebpackPlugin({
-          cacheDirectory: path.join(antwarPath, 'build-cache'),
-          recordsPath: path.join(antwarPath, 'build-cache', 'records.json'),
-          environmentHash: {
-            root: cwd,
-            directories: ['node_modules'],
-            files: ['package.json']
-          }
-        })
-      ]
+      }
     };
 
     return merge(commonConfig, buildConfig, config.webpack);
--- packages/antwar/src/config/dev.js
+++ packages/antwar/src/config/dev.js
@@ -1,14 +1,10 @@
 import * as path from 'path';
 import merge from 'webpack-merge';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import HardSourceWebpackPlugin from 'hard-source-webpack-plugin';
 import getCommon from './common';
 
-module.exports = function (config) {
-  const cwd = process.cwd();
-  const antwarPath = path.join(cwd, '.antwar');
-
-  return getCommon(config).then(function (commonConfig) {
+module.exports = (config) => (
+  getCommon(config).then(function (commonConfig) {
     const devConfig = {
       cache: true,
       node: {
@@ -16,7 +12,7 @@ module.exports = function (config) {
         fs: 'empty'
       },
       output: {
-        path: path.join(cwd, './.antwar/build/'),
+        path: path.join(process.cwd(), './.antwar/build/'),
         filename: '[name].js',
         publicPath: '/',
         chunkFilename: '[chunkhash].js'
@@ -26,15 +22,6 @@ module.exports = function (config) {
         new HtmlWebpackPlugin({
           template: (config.antwar.template && config.antwar.template.file) ||
             path.join(__dirname, '../../templates/page.ejs')
-        }),
-        new HardSourceWebpackPlugin({
-          cacheDirectory: path.join(antwarPath, 'dev-cache'),
-          recordsPath: path.join(antwarPath, 'dev-cache', 'records.json'),
-          environmentHash: {
-            root: cwd,
-            directories: ['node_modules'],
-            files: ['package.json']
-          }
         })
       ],
       // Copy template configuration to webpack side so HtmlWebpackPlugin picks it up
@@ -46,5 +33,5 @@ module.exports = function (config) {
     };
 
     return merge(commonConfig, devConfig, config.webpack);
-  });
-};
+  })
+);
