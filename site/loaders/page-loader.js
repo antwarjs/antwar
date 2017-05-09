b0d122aadd439e8e5ebc3a1a4d856db2189aacd2
Juho Vepsalainen <bebraw@gmail.com>
2017-05-09 15:06:35 +0300

    chore: Sketch out more data generators for a page loader

--- site/loaders/page-loader.js
+++ site/loaders/page-loader.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const frontmatter = require('front-matter');
 const markdown = require('../utils/markdown');
 const highlight = require('../utils/highlight');
@@ -7,6 +8,39 @@ module.exports = function (source) {
 
   result.attributes = result.attributes || {};
   result.body = markdown().process(result.body, highlight);
+  result.preview = generatePreview(result);
+  result.description = generateDescription(result);
+  result.keywords = generateKeywords(result);
 
   return `module.exports = ${JSON.stringify(result)};`;
 };
+
+function generatePreview(file) {
+  if (file.attributes && file.attributes.preview) {
+    return file.attributes.preview;
+  }
+
+  return file.body && file.body.slice(0, 100) + '…';
+}
+
+function generateDescription(file) {
+  if (file.attributes) {
+    return file.attributes.description || file.attributes.preview;
+  }
+
+  return file.preview;
+}
+
+function generateKeywords(file) {
+  let keywords;
+
+  if (file.attributes && file.attributes.keywords) {
+    keywords = file.attributes.keywords;
+  }
+
+  if (_.isString(keywords)) {
+    return keywords.split(',');
+  }
+
+  return keywords;
+}
