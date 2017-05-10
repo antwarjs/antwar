f912fd99276b85098204de0654bc2611cbc5a305
Juho Vepsalainen <bebraw@gmail.com>
2017-05-10 10:22:47 +0300

    fix - Pass missing `config` to getAllPages

--- packages/antwar/src/build/site.js
+++ packages/antwar/src/build/site.js
@@ -1,4 +1,6 @@
+const config = require('config'); // Alias by webpack
+
 module.exports = {
   renderPage: require('./render_page'),
-  allPages: require('../core/paths').getAllPages()
+  allPages: require('../core/paths').getAllPages(config)
 };
