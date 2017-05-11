8b17b78e94700d0285d5945bd568471771647370
Juho Vepsalainen <bebraw@gmail.com>
2017-05-11 13:31:11 +0300

    chore: Remove a redundant check

--- packages/antwar/src/core/paths/parse-layout.js
+++ packages/antwar/src/core/paths/parse-layout.js
@@ -1,6 +1,6 @@
 module.exports = function parseLayout(section, sectionName, layoutName) {
   return section.paths && section.paths[sectionName] && section.paths[sectionName].layouts &&
-    section.paths[sectionName].layouts && section.paths[sectionName].layouts[layoutName] ?
+    section.paths[sectionName].layouts[layoutName] ?
     section.paths[sectionName].layouts[layoutName]() :
     section.layouts[layoutName]();
 };
