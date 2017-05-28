275856705ac936dbeec6dfa9df35a81371a14188
Juho Vepsalainen <bebraw@gmail.com>
2017-05-28 12:19:19 +0300

    chore: Expand smoke test

--- smoketest/antwar.config.js
+++ smoketest/antwar.config.js
@@ -17,6 +17,17 @@ module.exports = () => ({
     },
     standalone: {
       index: () => require('./layouts/Standalone').default
+    },
+    demo: {
+      content: () => (
+        require.context(
+          './loaders/page-loader!./pages',
+          true,
+          /^\.\/.*\.md$/
+        )
+      ),
+      layout: () => require('./layouts/Page').default,
+      url: ({ sectionName, fileName }) => `/${sectionName}/${fileName}/`
     }
   }
 });
---
+++ smoketest/pages/configuration/nested/page.md
@@ -0,0 +1,5 @@
+---
+title: Nested page
+---
+Nested page
+
