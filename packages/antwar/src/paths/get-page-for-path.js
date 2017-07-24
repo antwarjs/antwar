923c4cec752891149a845231495d3924c85f7eac
Juho Vepsalainen <bebraw@gmail.com>
2017-07-24 11:21:05 +0200

    fix: Remove a redundant slash check

--- packages/antwar/src/paths/get-page-for-path.js
+++ packages/antwar/src/paths/get-page-for-path.js
@@ -1,12 +1,4 @@
 module.exports = function getPageForPath(config, path, pages) {
-  // XXXXX: Push this check to react-router definition
-  // A path should end in /
-  if (path.slice(-1) !== '/') {
-    console.warn('getPageForPath - No slash!', path, Object.keys(pages).join(', '));
-
-    return {};
-  }
-
   if (path === '/') {
     return pages['/'] || {};
   }
