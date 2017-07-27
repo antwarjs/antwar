47235cf00c79d0784ee2339efb37c846c3bdf1cb
Juho Vepsalainen <bebraw@gmail.com>
2017-07-27 14:55:17 +0200

    fix - Allow pages without a trailing slash work in development

--- packages/antwar/src/paths/get-page-for-path.js
+++ packages/antwar/src/paths/get-page-for-path.js
@@ -3,7 +3,7 @@ module.exports = function getPageForPath(config, path, pages) {
     return pages['/'] || {};
   }
 
-  const ret = pages[path];
+  const ret = pages[path] || pages[`${path}/`];
 
   if (!ret) {
     console.warn('getPageForPath - No match!', path, Object.keys(pages).join(', '));
