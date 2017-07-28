6b10fc733c2707ccf6be9aab1bfbc277c447a4ce
Juho Vepsalainen <bebraw@gmail.com>
2017-07-28 14:33:50 +0200

    chore: Drop special index handling from `getSectionPages`
    No need for that anymore.

--- packages/antwar/src/paths/get-section-pages.js
+++ packages/antwar/src/paths/get-section-pages.js
@@ -1,21 +1,6 @@
 const _ = require('lodash');
 
 module.exports = function getSectionPages(config, sectionName, pages) {
-  if (sectionName === '/') {
-    const paths = config.paths['/'].content();
-
-    if (paths.keys) {
-      return _.uniq(paths.keys().map(
-        k => ({
-          url: _.trim(k.split('.')[1], '/')
-        })
-      ));
-    }
-
-    // An index page
-    return [paths];
-  }
-
   return _.filter(
     pages,
     o => o.sectionName === sectionName && o.type === 'page'
