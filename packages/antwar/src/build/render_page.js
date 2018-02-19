694ce674f852d97cc39b618bfe2a4c57371a430f
Juho Vepsalainen <bebraw@gmail.com>
2018-02-19 11:34:53 +0100

    chore: Tidy up code

--- packages/antwar/src/build/render_page.js
+++ packages/antwar/src/build/render_page.js
@@ -7,13 +7,11 @@ import BodyContent from "../BodyContent";
 
 // TODO: what if a route isn't found?
 module.exports = function renderPage(location, cb) {
-  const context = {};
-
   const allPages = paths.getAllPages(config);
   const page = paths.getPageForPath(location, allPages);
 
   const html = ReactDOMServer.renderToStaticMarkup(
-    <StaticRouter location={location} context={context}>
+    <StaticRouter location={location} context={{}}>
       <Route component={BodyContent(page, allPages)} />
     </StaticRouter>
   );
